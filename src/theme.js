40d0eeee87ce73393f525afe168e4197d1e7a8fe
Yuxin Ye <yuxin_ye@protonmail.com>
2022-10-27 16:13:04 +1100

    add in typography and context for color mode

--- src/theme.js
+++ src/theme.js
@@ -1,6 +1,6 @@
 // Allow light or dark mode of the dashboard
 
-import { createContext, useState, useMemo } from "react";
+import React, { createContext, useState, useMemo } from "react";
 import { createTheme } from "@mui/material/styles";
 
 // color design token
@@ -163,5 +163,51 @@ export const themeSettings = (mode) => {
             },
           }),
     },
+    typography: {
+      fontFamily: ["Source Sans Pro", "sans-serif"].join(","),
+      fontSize: 12,
+      h1: {
+        fontFamily: ["Source Sans Pro", "sans-serif"].join(","),
+        fontSize: 40,
+      },
+      h2: {
+        fontFamily: ["Source Sans Pro", "sans-serif"].join(","),
+        fontSize: 32,
+      },
+      h3: {
+        fontFamily: ["Source Sans Pro", "sans-serif"].join(","),
+        fontSize: 24,
+      },
+      h4: {
+        fontFamily: ["Source Sans Pro", "sans-serif"].join(","),
+        fontSize: 20,
+      },
+      h5: {
+        fontFamily: ["Source Sans Pro", "sans-serif"].join(","),
+        fontSize: 16,
+      },
+      h6: {
+        fontFamily: ["Source Sans Pro", "sans-serif"].join(","),
+        fontSize: 14,
+      },
+    },
   };
+};
+
+// context for the color mode
+export const ColorModeContext = createContext({
+  toggleColorMode: () => {},
+});
+
+export const useMode = () => {
+  const [mode, setMode] = useState("dark");
+  const colorMode = useMemo(
+    () => ({
+      toggleColorMode: () =>
+        setMode((prev) => (prev === "light" ? "dark" : "light")),
+    }),
+    []
+  );
+  const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
+  return [theme, colorMode];
 };
