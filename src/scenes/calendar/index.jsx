d1295ac36162fa7572775acd5714bc40866b0dce
Yuxin Ye <yuxin_ye@protonmail.com>
2022-10-28 16:54:50 +1100

    add in fullcalendar

---
+++ src/scenes/calendar/index.jsx
@@ -0,0 +1,46 @@
+import { useState } from "react";
+import FullCalendar, { formatDate } from "@fullcalendar/core";
+import dayGridPlugin from "@fullcalendar/daygrid";
+import timeGridPlugin from "@fullcalendar/timegrid";
+import interactionPlugin from "@fullcalendar/interaction";
+import listPlugin from "@fullcalendar/list";
+import {
+  Box,
+  List,
+  ListItem,
+  ListItemText,
+  Typography,
+  useTheme,
+} from "@mui/material";
+import Header from "../../components/Header";
+import { tokens } from "../../theme";
+
+const Calendar = () => {
+  const theme = useTheme();
+  const colors = tokens(theme.palette.mode);
+  const [currentEvents, setCurrentEvents] = useState([]);
+
+  const handleDateClick = (selected) => {
+    const title = prompt("Plesae enter a new title for your event");
+    const calendarApi = selected.view.calendar;
+    calendarApi.unselect();
+
+    if (title) {
+      calendarApi.addEvent({
+        id: `${selected.dateStr}-${title}`,
+        title,
+        start: selected.dateStr,
+        end: selected.endStr,
+        allDay: selected.allDay,
+      });
+    }
+  };
+
+  return (
+    <Box m="20px">
+      <Header title="calendar" subtitle="here you go your awesome calendar" />
+    </Box>
+  );
+};
+
+export default Calendar;
