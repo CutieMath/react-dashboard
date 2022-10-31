fd95dde58e3f458253ce9cedd1289d6dcff47938
Yuxin Ye <yuxin_ye@protonmail.com>
2022-10-31 15:20:53 +1100

    Add lineChart into the dashboard

---
+++ src/scenes/line/index.jsx
@@ -0,0 +1,9 @@
+import LineChart from "../../components/LineChart";
+import { Box } from "@mui/material";
+import Header from "../../components/Header";
+
+const Line = () => {
+  return <LineChart />;
+};
+
+export default Line;
