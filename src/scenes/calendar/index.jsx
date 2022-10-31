36ad97144e423f2191bbd37a980a75de7ce4d5b2
Yuxin Ye <yuxin_ye@protonmail.com>
2022-10-31 12:00:36 +1100

    bugfix

--- src/scenes/calendar/index.jsx
+++ src/scenes/calendar/index.jsx
@@ -21,14 +21,15 @@ const Calendar = () => {
   const [currentEvents, setCurrentEvents] = useState([]);
 
   const handleDateClick = (selected) => {
-    const title = prompt("Plesae enter a new title for your event");
+    const title = prompt("Please enter a new title for your event");
     const calendarApi = selected.view.calendar;
     calendarApi.unselect();
+
     if (title) {
       calendarApi.addEvent({
         id: `${selected.dateStr}-${title}`,
         title,
-        start: selected.dateStr,
+        start: selected.startStr,
         end: selected.endStr,
         allDay: selected.allDay,
       });
