7f2213553177227ae22ddd73af321e44e598fc7c
Yuxin Ye <yuxin_ye@protonmail.com>
2022-10-31 15:44:21 +1100

    finish off all charts

--- src/scenes/geography/index.jsx
+++ src/scenes/geography/index.jsx
@@ -1,14 +1,24 @@
 import Choropleth from "../../components/Choropleth";
 import { Box } from "@mui/material";
 import Header from "../../components/Header";
+import { tokens } from "../../theme";
+import { useTheme } from "@mui/material";
 
 const Geography = () => {
+  const theme = useTheme();
+  const colors = tokens(theme.palette.mode);
+
   return (
     <Box>
       <Box m="20px">
         <Header title="Choropleth" subtitle="here is your geography graph" />
       </Box>
-      <Box height="75vh">
+      <Box
+        height="75vh"
+        border={`1px solid ${colors.grey[100]}`}
+        borderRadius="4px"
+        m="20px"
+      >
         <Choropleth />
       </Box>
     </Box>
