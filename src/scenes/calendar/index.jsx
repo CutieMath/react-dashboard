bd7fd25e635b365dee9a30f82bbff042c4cddaf5
Yuxin Ye <yuxin_ye@protonmail.com>
2022-10-31 11:23:08 +1100

    create calendar sidebar

--- src/scenes/calendar/index.jsx
+++ src/scenes/calendar/index.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import FullCalendar, { formatDate } from "@fullcalendar/core";
+import FullCalendar, { formatDate } from "@fullcalendar/react";
 import dayGridPlugin from "@fullcalendar/daygrid";
 import timeGridPlugin from "@fullcalendar/timegrid";
 import interactionPlugin from "@fullcalendar/interaction";
@@ -34,11 +34,57 @@ const Calendar = () => {
         allDay: selected.allDay,
       });
     }
+
+    const handleDeleteEvent = (selected) => {
+      if (
+        window.confirm(
+          `Are you sure you want to delete this event '${selected.event.title}'? `
+        )
+      ) {
+        selected.event.remove();
+      }
+    };
   };
 
   return (
     <Box m="20px">
       <Header title="calendar" subtitle="here you go your awesome calendar" />
+      <Box display="flex" justifyContent="space-between">
+        {/* Calendar Sidebar  */}
+        <Box
+          flex="1 1 20%"
+          backgroundColor={colors.primary[400]}
+          p="15px"
+          borderRadius="4px"
+        >
+          <Typography variant="h5">Events</Typography>
+          <List>
+            {currentEvents.map((event) => (
+              <ListItem
+                key={event.id}
+                sx={{
+                  backgroundColor: colors.greenAccent[500],
+                  margin: "10px 0",
+                  borderRadius: "4px",
+                }}
+              >
+                <ListItemText
+                  primary={event.title}
+                  secondary={
+                    <Typography>
+                      {formatDate(event.start, {
+                        year: "numeric",
+                        month: "short",
+                        day: "numeric",
+                      })}
+                    </Typography>
+                  }
+                ></ListItemText>
+              </ListItem>
+            ))}
+          </List>
+        </Box>
+      </Box>
     </Box>
   );
 };
