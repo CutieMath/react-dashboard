34ebd33bcffd53e74244e266b6e4a1216c74df2a
Yuxin Ye <yuxin_ye@protonmail.com>
2022-10-31 12:09:06 +1100

    start to implement faq

---
+++ src/scenes/faq/index.js
@@ -0,0 +1,13 @@
+import { Box, useTheme, Typography } from "@mui/material";
+import Header from "../../components/Header";
+import Accordion from "@mui/material/Accordion";
+import AccordionSummary from "@mui/material/AccordionSummary";
+import AccordionDetails from "@mui/material/AccordionDetails";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+import { tokens } from "../../theme";
+
+const FAQ = () => {
+  return <div>FAQ</div>;
+};
+
+export default FAQ;
